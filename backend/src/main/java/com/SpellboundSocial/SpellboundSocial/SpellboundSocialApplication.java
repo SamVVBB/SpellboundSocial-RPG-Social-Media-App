5ca4d52df10faa5850e19aa20bbd6b2f586c5790
SamVVBB <hedinsamuel1995@gmail.com>
2023-03-27 05:20:50 -1000

    Initial commit

---
+++ backend/src/main/java/com/SpellboundSocial/SpellboundSocial/SpellboundSocialApplication.java
@@ -0,0 +1,13 @@
+package com.SpellboundSocial.SpellboundSocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellboundSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpellboundSocialApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/SpellboundSocial/SpellboundSocial/SpellboundSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpellboundSocial.SpellboundSocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpellboundSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
